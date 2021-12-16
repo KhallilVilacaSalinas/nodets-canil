a07995c1509aab3db458e85a1f3322b4d4ceacb5
KhallilVSalinas <khallil@tetracon.com.br>
2021-12-15 21:29:00 -0300

    Projeto final

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from 'express';
-
+import { Pet } from '../models/pat';
 import { createMenuObject } from '../helpers/createMenuObject';
 
 
 export const home = (req: Request, res: Response) => {
+  let list = Pet.getAll();
+
   res.render('pages/page', {
+    list,
     menu: createMenuObject('all'),
     bunner: {
       title: 'Todos os animais',
@@ -14,32 +17,41 @@ export const home = (req: Request, res: Response) => {
 };
 
 export const dogs = (req: Request, res: Response) => {
+  let list = Pet.getType('dog');
+
   res.render('pages/page', {
     menu: createMenuObject('dog'),
     bunner: {
       title: 'Cachorros',
       background: 'banner_dog.jpg',
     },
+    list
   });
 };
 
 export const cats = (req: Request, res: Response) => {
+  let list = Pet.getType('cat');
+  
   res.render('pages/page', {
     menu: createMenuObject('cat'),
     bunner: {
       title: 'Gatos',
       background: 'banner_cat.jpg',
     },
+    list
   });
 };
 
 export const fishes = (req: Request, res: Response) => {
+  let list = Pet.getType('fish');
+
   res.render('pages/page', {
     menu: createMenuObject('fish'),
     bunner: {
       title: 'Peixes',
       background: 'banner_fish.jpg',
     },
+    list
   });
 };
 
--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,21 @@
 import { Request, Response } from 'express';
+import { createMenuObject } from '../helpers/createMenuObject';
+
+import { Pet } from '../models/pat';
 
 export const search = (req: Request, res: Response) => {
-  res.send("search")
+  let name: string = req.query.name as string;
+  let list = Pet.getFromName(name);
+
+  if (!name) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('pages/page', {
+    menu: createMenuObject(''),
+    list,
+    name
+  });
+  console.log('foi')
 };
